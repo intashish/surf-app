6c3e8a9ed348124349638e7cd605b6c023bf5109
Ashish Singh <ashish.s@purplle.com>
2022-07-04 18:16:21 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop Home' });
 });
 
+/* GET /register. */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST /register */
+router.get('/', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* GET /login */
+router.post('/', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST /login */
+router.get('/', (req, res, next) => {
+  res.send('POST /login');
+});
+
+/* GET /profile */
+router.get('/', (req, res, next) => {
+  res.send('POST /login');
+});
+
 module.exports = router;
